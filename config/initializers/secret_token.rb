ec87e4b81c543995287b78b9fac627c27c8dd067
liormb <liormb@gmail.com>
2014-05-08 18:04:32 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EchonestArtists::Application.config.secret_key_base = '0f990dca331c89b6fd7f482321b4553ab2e551c807994aaf78510b8afe991b66cba9d463b8611f427fcd22066e7eecb72282f1291ce41bf4ebaa6cde7806656d'
