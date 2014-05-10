d40a88e02769d01d0710064b897a0a6ed2e160c5
liormb <liormb@gmail.com>
2014-05-10 13:18:23 -0400

    removing angular-resourse.min.js

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('EchoNestArtistsApp',["ngResource"]);
+var app = angular.module('EchoNestArtistsApp',[]);
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require jquery_ujs
 //= require bootstrap.min
 //= require angular.min
-//= require angular-resource.min
 //= require turbolinks
 //= require_tree .
